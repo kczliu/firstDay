6e14968cb1713dee684854659987a43b7f55d25a
kczliu <kczliu@163.com>
2019-06-06 10:24:55 +0800

    Revert "qq"
    This reverts commit 50c211716d646f7f25cbbbe772d1dd0687b81dbd.

--- client/component/sider/index.js
+++ client/component/sider/index.js
@@ -3,77 +3,17 @@ import { Menu, Icon, Button } from 'antd';
 import "antd/dist/antd.css"
 import './styles.css'
 import {inject,observer} from 'mobx-react'
-import sideMenu from '../../config/router'
-import {withRouter} from 'react-router-dom'
 const SubMenu = Menu.SubMenu;
 
 class Sider extends Component{
     render(){
         return (
-            <div id='sider' style={this.props.subMenu.collapes?{width:"80px"}:{width:"200px"}}>
-                <a className='github' href="https://github.com/treasureDouDou">
-                    <svg
-                        aria-hidden="true"
-                        height="32"
-                        version="1.1"
-                        viewBox="0 0 16 16"
-                        width="32"
-                    >
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                    </svg>
-                    {this.props.subMenu.collapes ? '' : <span>{this.props.userState.user.userInfo.userName} </span>}
-                </a>
-                <Menu
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    mode="inline"
-                    theme="dark"
-                    inlineCollapsed={this.props.subMenu.collapes}
-                >
-                    <Menu.Item key="1">
-                        <Icon type="pie-chart" />
-                        <span>Option 1</span>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Icon type="desktop" />
-                        <span>Option 2</span>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Icon type="inbox" />
-                        <span>Option 3</span>
-                    </Menu.Item>
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
-                </Menu>
-            </div>
+            <div id='sider' style={this.props.subMenu.collapes?{width:"80px"}:{width:"200px"}}>side menu</div>
         )
     }
 }
 
-export default withRouter((inject(stores => {return {
-    subMenu: stores.submenu,
-    userState: stores.userState
-}})(observer(Sider))))
 
-
-const mapRouter =(routers)=>{
-    routers.map(item=>{
-        if(item.children){
-            mapRouter(item)
-        }else{
-
-        }
-    })
-}
+export default (inject(stores => {return {
+    subMenu: stores.submenu
+}})(observer(Sider)))
